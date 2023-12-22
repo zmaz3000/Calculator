7c92fe898d294cc89c0096be878b4da427dc53e2
zmaz3000 <zmaz3000@gmail.com>
2023-12-22 09:03:40 -0600

    added calculation functionality

--- app.js
+++ app.js
@@ -1,28 +1,104 @@
-var btn = document.querySelectorAll("button");
+var btn = document.querySelectorAll(".number");
+var operate = document.querySelectorAll(".operand")
 var display = document.querySelector(".screentwo");
+display.innerHTML = '';
 var clear = document.querySelector("#clear");
-var deleted = document.querySelector("#delete");
-var activeflag = true;
-var updateddisplay;
+var equal = document.querySelector(".equal");
+var remove = document.querySelector("#delete");
 
 for(var i = 0; i<btn.length; i++){
     btn[i].addEventListener("click", (e)=>{
-        if(e.target.classList.contains("number") == true){
-           display.innerHTML += e.target.innerHTML;
-            
-            
-        }
-
+        display.innerHTML += e.target.innerHTML
        
+        
+    }
+)}
+
+for(var i = 0; i<operate.length; i++){
+    operate[i].addEventListener("click", (e) => {
+        numberOne = parseInt(display.innerHTML)
+            var operand = e.target.innerHTML;
+            display.innerHTML = '';
+            equals(numberOne, operand)
+            
+
+            
+            
+    })
+}
+
+
+
+function equals(n, operand){
+    equal.addEventListener("click", () => {
+        var numberTwo = parseInt(display.innerHTML);
+        doSomething(n, numberTwo, operand)
+        n=0;
+        operand = null;
         
     })
 }
 
+
+
+function doSomething(number, numberTwo, operation){
+    console.log("The number is " + number + " and the operand is " + operation);
+    
+            
+            switch(operation){
+                case '+':
+                    add(number, numberTwo);
+                    break;
+                case '-':
+                    subract(number, numberTwo);
+                    break;
+                case '/':
+                    divide(number, numberTwo);
+                    break;
+                case 'x':
+                    multiply(number, numberTwo);
+                    break;
+            }
+}
+        
+    
+
+
+function add(n, n2){
+    console.log(n, n2)
+    display.innerHTML = n + n2;
+    return;
+}
+
+function subtract(n, n2){
+    display.innerHTML = n - n2;
+    return;
+}
+
+function divide(n, n2){
+    display.innerHTML = n / n2;
+    return;
+}
+
+function multiply(n, n2){
+    display.innerHTML = n * n2;
+    return;
+}
+
 clear.addEventListener("click", ()=>{
     display.innerHTML = '';
+    return;
 })
 
-deleted.addEventListener("click", ()=>{
-    var deletedresult = display.innerHTML.slice(0,-1);
-    display.innerHTML = deletedresult;
-})
+remove.addEventListener("click", ()=>{
+    var log = display.innerHTML.split('');
+    log.pop();
+    var newArray = log.join("");
+    display.innerHTML = newArray;
+ })
+
+
+
+
+
+
